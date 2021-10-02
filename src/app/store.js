7ce23de4e3903b39e4c88665e0bc5cc2661a0e29
Angel Wiki <wikiyodo@gmail.com>
2021-10-02 16:26:33 +0100

    link metmuseum store to the app store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import museumReducer from "../features/metmuseum.store";
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    museum: museumReducer
+  }
 });
